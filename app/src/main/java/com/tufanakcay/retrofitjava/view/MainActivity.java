b41811e0afd6d0a9c584418c5bc05982f28a4632
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-17 23:06:44 +0300

    Update commit

--- app/src/main/java/com/tufanakcay/retrofitjava/view/MainActivity.java
+++ app/src/main/java/com/tufanakcay/retrofitjava/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.tufanakcay.retrofitjava;
+package com.tufanakcay.retrofitjava.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.tufanakcay.retrofitjava.R;
 
 public class MainActivity extends AppCompatActivity {
 
